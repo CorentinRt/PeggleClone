94b84940089c2cc12719f24fac228355bdfb9506
BVerstaen <bverstaen04@gmail.com>
2023-10-17 19:30:02 +0200

    Fix UIScript

--- PeggleClone/Assets/Scripts/UIscript.cs
+++ PeggleClone/Assets/Scripts/UIscript.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UIscript : MonoBehaviour
+public class UIScript : MonoBehaviour
 {
+    public static UIScript instance;
+
     [SerializeField] TMP_Text _ballText;
     [SerializeField] TMP_Text _scoreText;
+
+    private void Awake()
+    {
+        if (instance != null) Destroy(gameObject); 
+        instance = this;
+    }
 
     public void UpdateBallText(int numberBalls)
     {
         _ballText.text = numberBalls.ToString();
     }
 
-    public void UpdateScoreText(int newScore) 
+    public void UpdateScoreText(int newScore)
     {
         _scoreText.text = newScore.ToString();
     }
