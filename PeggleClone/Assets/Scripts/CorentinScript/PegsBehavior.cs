de195d68a2535ab335997f61bc901de63cf7cf4b
Corentinrmt <corentin.remot@gmail.com>
2023-10-17 11:06:44 +0200

    Tag Creation and peggle destruction script
    tag balle and peggle
    started script PeggleBehavior

---
+++ PeggleClone/Assets/Scripts/CorentinScript/PegsBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegsBehavior : MonoBehaviour
+{
+    [Header("Gestion collision")]
+    [SerializeField] private bool _hasBeenTouched;
+
+    [Header("Gestion Visual")]
+    [SerializeField] private Sprite _normalSprite;
+    [SerializeField] private Sprite _spriteTouched;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
