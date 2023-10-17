81d51ffcea7a966e46c4e051438db840a26d7ddb
Corentinrmt <corentin.remot@gmail.com>
2023-10-17 11:25:11 +0200

    Peggle Prefab Creation and Script Peggle
    Prefab PeggleSquare and PeggleCircle
    Script Peggle Behavior (destruction if ball stay too long near the peggle)

--- PeggleClone/Assets/Scripts/CorentinScript/PegsBehavior.cs
+++ PeggleClone/Assets/Scripts/CorentinScript/PegsBehavior.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class PegsBehavior : MonoBehaviour
 {
+    // Fields
     [Header("Gestion collision")]
     [SerializeField] private bool _hasBeenTouched;
+
+    [SerializeField] private float _preDestructionTime;
+    [SerializeField] private float _currentPreDestructionTime;
 
     [Header("Gestion Visual")]
     [SerializeField] private Sprite _normalSprite;
     [SerializeField] private Sprite _spriteTouched;
 
+    // Properties
+
+
+    // Methods
+
+    private void DestructPeggle()
+    {
+        Destroy(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _hasBeenTouched = false;
     }
 
     // Update is called once per frame
@@ -23,5 +37,26 @@ public class PegsBehavior : MonoBehaviour
         
     }
 
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Balle") && !_hasBeenTouched)
+        {
+            _hasBeenTouched = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("stay");
+        if(collision.gameObject.CompareTag("Balle") && _hasBeenTouched)
+        {
+            _currentPreDestructionTime += Time.deltaTime;
+            
+            if(_currentPreDestructionTime >= _preDestructionTime)
+            {
+                DestructPeggle();
+            }
+        }
+    }
+
 }
